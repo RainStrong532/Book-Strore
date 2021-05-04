c9745de4d7cb99fa5c919438924e26c7d6d85dc6
Manh vu <vuvanmanhttvtvp2@gmail.com>
2021-05-04 23:22:52 +0700

    add middlewares

--- expressjs/src/middlewares/verifySignup.js
+++ expressjs/src/middlewares/verifySignup.js
@@ -1,17 +1,16 @@
-const User = require('../data/account');
+const {Account} = require('../models');
 const { ROLES } = require('../../contants');
 
-checkDuplicateUsername = async (req, res, next) => {
-    // Username
-    const username = req.body.username;
-    if (!username) {
-        res.status(400).json({ message: 'Thiếu tên tài khoản' });
+const checkDuplicateUsername = async (req, res, next) => {
+    const user_name = req.body.user_name;
+    if (!user_name) {
+        res.status(400).json({ message: 'Username is required'});
         return;
     }
     try {
-        const isExisted = await User.existedUser(req.body.username);
+        const isExisted = await Account.existedUserName(user_name);
         if (isExisted) {
-            res.status(200).json({ message: "Tài khoản đã tồn tại" });
+            res.status(200).json({ message: "Username already exists" });
             return;
         } else {
             next();
@@ -22,21 +21,21 @@ checkDuplicateUsername = async (req, res, next) => {
     }
 };
 
-checkRolesExisted = (req, res, next) => {
-    if (req.body.roles) {
-        for (let i = 0; i < req.body.roles.length; i++) {
-            if (!ROLES[req.body.roles[i]]) {
+const checkRolesExisted = (req, res, next) => {
+    let roles = req.body.roles;
+
+    if (roles) {
+        for (let i = 0; i < roles.length; i++) {
+            if (!ROLES[roles[i]]) {
                 res.status(400).json({
-                    message: "Failed! Role does not exist = " + req.body.roles[i]
+                    message: "Failed! " + roles[i] + " does not exist"
                 });
                 return;
             }
         }
     }else{
-        res.status(400).json({
-            message: "Role is required!"
-        });
-        return;
+        roles = [ROLES.USER];
+        req.body.roles = [ROLES.USER];
     }
     next();
 };
@@ -45,7 +44,8 @@ function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
-checkMail = async (req, res, next) => {
+
+const checkMail = async (req, res, next) => {
     const email = req.body.email;
     if (!email) {
         res.status(400).json({ message: 'Email is required' });
@@ -56,7 +56,7 @@ checkMail = async (req, res, next) => {
         return;
     }
     try {
-        const isExisted = await User.existedEmail(email);
+        const isExisted = await Account.existedEmail(email);
         if (isExisted) {
             res.status(200).json({ message: "Email was existed"});
             return;
@@ -64,6 +64,7 @@ checkMail = async (req, res, next) => {
             next();
         }
     } catch (err) {
+        console.log(err);
         res.status(400).json(err);
         return;
     }
