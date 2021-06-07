6e237510da12ec4104bab1d1aa515a36ccc695e3
Manh vu <vuvanmanhttvtvp2@gmail.com>
2021-06-07 14:54:56 +0700

    refactor

--- react-app/src/components/commons/Footer.js
+++ react-app/src/components/commons/Footer.js
@@ -18,7 +18,7 @@ function Footer() {
 						<h2 className="h2-1">Contact Info</h2>
 
 						<ul className="ul alt">
-							<li><span className="fa fa-envelope-o"></span> <a href="#">contact@company.com</a></li>
+							<li><span className="fa fa-envelope"></span> <a href="#">contact@company.com</a></li>
 							<li><span className="fa fa-phone"></span> +1 333 4040 5566 </li>
 							<li><span className="fa fa-map-pin"></span> 212 Barrington Court New York, ABC 10001 United States of America</li>
 						</ul>
--- react-app/src/containers/ChatContainer.js
+++ react-app/src/containers/ChatContainer.js
@@ -1,14 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
 import Chat from '../components/chat/Chat'
-import Cookies from 'js-cookie';
-import * as urls from '../services/url';
-import fetchApi from '../services/fetchApi'
-import { useHistory } from 'react-router';
 
 
 function AuthorUpdate() {
-    const history = useHistory();
 
     return (
         <Container style={{ background: "#FFF", borderRadius: "8px" }} className="my-3 pb-3 d-flex flex-column">
